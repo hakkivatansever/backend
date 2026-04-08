dbedb5b24f750322527ed8aaa76fe72043ca94e4
Hakka <hakkahalmiev@gmail.com>
2026-04-08 21:48:53 +0100

    Db connection, register, login

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-app = FastAPI()
+from database import SessionLocal, engine, Base
+import models, schemas, auth
 
-@app.get("/")
-def home():
-    return {"message" : "API calisiyor"}
+Base.metadata.create_all(bind=engine)
+
+app= FastAPI()
+
+#Database Connection
+
+def get_db():
+    db= SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#Register
+
+@app.post("/register")
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed = auth.hash_password(user.password)
+    new_user = models.User(email=user.email, password=hashed)
+    db.add(new_user)
+    db.commit()
+    return {"message" : "User created"}
+
+#Login
+
+@app.post("/login")
+def login(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+
+    if not db_user or not auth.verify_password(user.passsword, db_user.password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+    
+    token = auth.create_token({"user_id": db_user.id})
+    return {"access_token":token}
+
